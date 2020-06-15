fe28addac8f37a298e915b92cb3f0ae583f2e1c3
Emmanuel Balpe <e.balpe@dev1-0.com>
2020-06-15 08:29:45 +0200

    feat: update bio

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ export default function Home({ allPostsData }) {
       <section>
         <h2>Welcome, my name is Manu !</h2>
         <p>I'm a enthusiast Web Developer. I've been working with Java for about 10 years and I've been really serious with JavaScript for 2 years now.
-          I love to learn and here on this blog I will share some of my dev experiments</p>
+          I love to learn new technologies, research and work through complex problems. I try to pay the highest attention to quality, I love to see my tests run green.</p>
+        <p>I also love sharing my knowledge with others and talk about it.</p>
       </section>
 
       <section>
