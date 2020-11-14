ec6a7efe7ceeced2e371a0ca177b90c18e9de41e
Emmanuel Balpe <m4nu56@gmail.com>
2020-11-14 15:42:57 +0100

    feat: replace favicons

--- components/layout.js
+++ components/layout.js
@@ -21,19 +21,19 @@ export default function Layout({ children, home }) {
   return (
     <div className={'container'}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Another developer personal blog..."
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <meta name="description" content={siteTitle}/>
         <meta
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
+            siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="og:title" content={siteTitle}/>
+        <meta name="twitter:card" content="summary_large_image"/>
       </Head>
       <header className={'container'}>
         {home ? (
