3ef3117d12e5c8dc7f8cd0ea2eb21c749fac6617
Emmanuel Balpe <m4nu56@gmail.com>
2020-11-15 17:38:54 +0100

    style: update global paddings

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,12 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import DateFormatter from '../components/DateFormatter'
 import { FaArrowRight } from 'react-icons/fa'
-import { name } from '../components/Layout'
 import React from 'react'
 
 export default function Home({ allPostsData }) {
   return (
     <>
-      <h1>{name}</h1>
+      <h1>m4nu56</h1>
       <section>
         <h2>Welcome, my name is Manu !</h2>
         <p>I'm a enthusiast Web Developer. I've been working with Java for about 10 years and I've been really serious with JavaScript for 2 years now.
@@ -22,7 +21,7 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, publishDate, title }) => (
             <li key={id} className={styles.listItem}>
               <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a className={styles.blogTitle}>
+                <a className={styles.postTitle}>
                   <span><FaArrowRight size={15}/>{title}</span>
                 </a>
               </Link>
