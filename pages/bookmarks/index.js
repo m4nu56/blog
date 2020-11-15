ffd9e66606f9fdd16159f8eda460a2d8b6109c09
Emmanuel Balpe <m4nu56@gmail.com>
2020-11-15 18:05:56 +0100

    post: having fun with perspective in css

--- pages/bookmarks/index.js
+++ pages/bookmarks/index.js
@@ -1,6 +1,12 @@
+import { TwitterTweetEmbed } from 'react-twitter-embed'
 
 export default function Bookmarks() {
   return (
-    <h1>Bookmarks</h1>
+    <>
+      <h1>Bookmarks</h1>
+      <TwitterTweetEmbed
+        tweetId={'1326916915479142401'}
+      />
+    </>
   )
 }
