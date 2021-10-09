9bb3ae615e251c02f2ca19ac5e02fda9b2109a8b
Emmanuel Balpe <m4nu56@gmail.com>
2021-10-09 15:39:08 +0000

    Update my name

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ allPostsData }) {
     <>
       <h1>m4nu56</h1>
       <section>
-        <h2>Welcome, my name is Manu !</h2>
+        <h2>Welcome, my name is Emmanuel Balpe !</h2>
         <p>I'm a enthusiast Web Developer. I've been working with Java for about 10 years and I've been really serious with JavaScript for 2 years now.
           I love to learn new technologies, research and work through complex problems. I try to pay the highest attention to quality, I love to see my tests run green.</p>
         <p>I also love sharing my knowledge with others and talk about it.</p>
